cd4f89065e54a2af4d51bb932de2fc3f6674fa2d
RandyZhang <zhangshi3421@gmail.com>
2022-11-20 12:44:41 +0800

    增加Prettier和eslint

---
+++ src/types/env.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../" />
+/// <reference path="./" />
--- webpack_config/webpack.config.eslint.js
+++ webpack_config/webpack.config.eslint.js
@@ -1,11 +1,11 @@
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
-    plugins: [
-        new ESLintPlugin({
-            fix: true /* 自动帮助修复 */,
-            extensions: ['js', 'json', 'coffee', 'vue'],
-            exclude: 'node_modules',
-        }),
-    ],
-};
+  plugins: [
+    new ESLintPlugin({
+      fix: true /* 自动帮助修复 */,
+      extensions: ['js', 'json', 'coffee', 'vue'],
+      exclude: 'node_modules',
+    }),
+  ],
+}
