80a9de55eed11080c77fd66d0b10cba57b9e2bb0
RandyZhang <zhangshi3421@gmail.com>
2022-11-20 14:51:32 +0800

    完成Router的配置，修改ts警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,9 @@ module.exports = {
      * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
      * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
      */
-
     // 关闭对所有未定义变量的警告提示
     'no-undef': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-multiple-template-root': 'off',
     // 对于requires引入 关闭
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': ['off'],
---
+++ src/pages/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Button>123</Button>
+  </div>
+</template>
+<script setup lang="ts">
+import { Button } from 'ant-design-vue'
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
