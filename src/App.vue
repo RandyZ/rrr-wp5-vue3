654889062ada4b8bde70eab833f11022a9401c2a
RandyZhang <zhangshi3421@gmail.com>
2022-11-20 14:51:32 +0800

    完成Router的配置，修改ts警告

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1><br>
-    <Button class="randy-css">123</Button>
-    <div>HostVal : {{ hostVal }}</div>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <Button>Ant-Design-Button</Button>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
+
 <script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
 import { Button } from 'ant-design-vue'
-const env = process.env
-
-console.log('Process Env', JSON.stringify(env))
-
-const hostVal = env.BASE_URL
+import { RouterLink, RouterView } from 'vue-router'
 </script>
-<style lang="less">
+<style lang="less" scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
@@ -23,6 +31,67 @@ const hostVal = env.BASE_URL
     .randy-css {
       color: red;
     }
+  }
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,164 +1,172 @@
-const path = require('path');
+const path = require('path')
 // 配置模板（HtmlWebpackPlugin）
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // CSS打包分离
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // CSS压缩
-const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 // 代码压缩
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 // Vue
-const { VueLoaderPlugin } = require('vue-loader');
+const { VueLoaderPlugin } = require('vue-loader')
 // Webpack插件
-const { DefinePlugin } = require('webpack');
+const { DefinePlugin } = require('webpack')
+
+const ESLintPlugin = require('eslint-webpack-plugin')
+
 // -----------------------------------------------可选项-------------------------------------------------
 // ----------------------------------------------参数变量-------------------------------------------------
-const NodeEnv = process.env.NODE_ENV;
-const DevMode = NodeEnv !== 'production';
-console.info('Randy ENV', process.env.NODE_ENV, 'mode', DevMode);
+const NodeEnv = process.env.NODE_ENV
+const DevMode = NodeEnv !== 'production'
+console.info('Randy ENV', process.env.NODE_ENV, 'mode', DevMode)
 module.exports = {
-    mode: 'development',
-    entry: './src/main.ts',
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 9000,
-        hot: true,
-        open: false,
+  mode: 'development',
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9000,
+    hot: true,
+    open: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+    extensions: ['*', '.js', '.ts', '.vue', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  // 优化编译
+  optimization: {
+    nodeEnv: false,
+    runtimeChunk: true,
+    moduleIds: 'deterministic',
+    minimizer: [
+      new TerserPlugin({
+        parallel: 4, //核心数量
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
         },
-        extensions: ['*', '.js', '.ts', '.vue', '.json'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true,
-    },
-    // 优化编译
-    optimization: {
-        nodeEnv: false,
-        runtimeChunk: true,
-        moduleIds: 'deterministic',
-        minimizer: [
-            new TerserPlugin({
-                parallel: 4, //核心数量
-                terserOptions: {
-                    parse: {
-                        ecma: 8,
-                    },
-                    compress: {
-                        ecma: 5,
-                        warnings: false,
-                        comparisons: false,
-                        inline: 2,
-                    },
-                    mangle: {
-                        safari10: true,
-                    },
-                    output: {
-                        ecma: 5,
-                        comments: false,
-                        ascii_only: true,
-                    },
-                },
-            }),
-        ],
-        // 分Chunk抽离重复代码
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all',
-            // 重复打包问题
-            cacheGroups: {
-                vendors: {
-                    // node_modules里的代码
-                    test: /[\\/]node_modules[\\/]/,
-                    chunks: 'all',
-                    // name: 'vendors', 一定不要定义固定的name
-                    priority: 10, // 优先级
-                    enforce: true,
-                },
-            },
-        },
-    },
-    module: {
-        rules: [
-            // 配置资源文件
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[hash][ext][query]',
-                },
-            },
-            // 配置样式文件
-            {
-                test: /\.css$/i,
-                use: [
-                    DevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    // 多进程编译
-                    {
-                        loader: 'thread-loader',
-                        options: { workerParallelJobs: 2 },
-                    },
-                ],
-            },
-            {
-                test: /\.less$/i,
-                use: [
-                    // less-loader-> postcss-loader -> css-loader，按照这个顺序执行，解决less特殊语法渗透到后续loader中
-                    DevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            lessOptions: {
-                                javascriptEnabled: true,
-                            },
-                        },
-                    },
-                ],
-            },
-            // Vue编译
-            {
-                test: /.vue$/,
-                use: ['vue-loader'],
-            },
-            // 1. ts编译支持；2. js兼容支持
-            {
-                test: /\.(t|j)s$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            vue: 'single-spa',
-        }),
-        new CssMinimizerWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash:8].css',
-        }),
-        new VueLoaderPlugin(),
-        new DefinePlugin({
-            'process.env': { ...require('./env').loadEnv(NodeEnv) },
-        }),
+      }),
     ],
-};
+    // 分Chunk抽离重复代码
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
+      // 重复打包问题
+      cacheGroups: {
+        vendors: {
+          // node_modules里的代码
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          // name: 'vendors', 一定不要定义固定的name
+          priority: 10, // 优先级
+          enforce: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      // 配置资源文件
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
+      // 配置样式文件
+      {
+        test: /\.css$/i,
+        use: [
+          DevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          // 多进程编译
+          {
+            loader: 'thread-loader',
+            options: { workerParallelJobs: 2 },
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // less-loader-> postcss-loader -> css-loader，按照这个顺序执行，解决less特殊语法渗透到后续loader中
+          DevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      // Vue编译
+      {
+        test: /.vue$/,
+        use: ['vue-loader'],
+      },
+      // 1. ts编译支持；2. js兼容支持
+      {
+        test: /\.(t|j)s$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      vue: 'single-spa',
+    }),
+    new CssMinimizerWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css',
+    }),
+    new VueLoaderPlugin(),
+    new DefinePlugin({
+      'process.env': { ...require('./env').loadEnv(NodeEnv) },
+    }),
+    new ESLintPlugin({
+      fix: true /* 自动帮助修复 */,
+      extensions: ['js', 'json', 'coffee', 'vue'],
+      exclude: 'node_modules',
+    }),
+  ],
+}
